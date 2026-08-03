fa027aba291b9995ab5f7f9b9daa12fbec915d61
10_Enendyta Naeswari Lintang D'Clara <clara.tkjsmeksa25@gmail.com>
2026-08-03 10:12:04 +0700

    Add files via upload

---
+++ Program Ganjil Genap.py
@@ -0,0 +1,7 @@
+angka = int(input("Masukkan sebuah angka: "))
+
+# Mengecek sisa bagi dengan 2
+if angka % 2 == 0:
+    print(f"Angka {angka} adalah bilangan genap.")
+else:
+    print(f"Angka {angka} adalah bilangan ganjil.")
