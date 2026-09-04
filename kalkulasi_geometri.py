8b2a71f2359b8559171b271bbf88a211f01d0469
10_Enendyta Naeswari Lintang D'Clara <clara.tkjsmeksa25@gmail.com>
2026-09-04 12:34:21 +0700

    kalkulasi_geometri

---
+++ kalkulasi_geometri.py
@@ -0,0 +1,12 @@
+def hitung_luas_persegi_v1(s):
+    return s * s
+
+def hitung_keliling_persegi_v1(s):
+    return 4 * s
+
+def hitung_luas_lingkaran_v1(r):
+    return 3.14 * r * r
+
+def hitung_luas_segitiga_v1(a, t):
+    return 0.5 * a * t
+    
